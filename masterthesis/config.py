748722e1f22590f62bc2d70fb366ac7a2de1f40c
Nick Zumbühl <nickzumbuhl@Nicks-MacBook-Air.local>
2020-01-07 10:29:39 +0100

    config file modified

--- masterthesis/config.py
+++ masterthesis/config.py
@@ -1,6 +1,16 @@
-# (1) SETTING UP ENVIRONMENT WITH EITHER ENVIRONMENT.YML FILE OR REQUIREMENTS.TXT
+"""
+Setting up Environment with 'requirements.txt':
+"""
 
-# pip install -r requirements.txt
+# 1. cd to the directory where requirements.txt is located.
+# 2. activate your virtualenv.
+# 3. run: pip install -r requirements.txt in your shell.
 
+"""
+Setting up Environment with 'environment.yml' file:
+"""
 
+# 1. conda env create -f environment.yml (creates a new environment based on the yml file)
+# 2. conda env list (lists all environments that exists on computer - check whether new environment is installed)
+# 3. activate environment in terminal (conda activate environment)
 
