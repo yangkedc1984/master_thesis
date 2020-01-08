801a163359528037a44857c219cb1d037e192edc
Nick Zumbühl <nickzumbuhl@Nicks-MacBook-Air.local>
2020-01-08 18:02:19 +0100

    Generating data set for HAR model complete

--- masterthesis/data_preprocessing.py
+++ masterthesis/data_preprocessing.py
@@ -5,7 +5,7 @@ import numpy as np
 plt.style.use('seaborn')
 
 # data loading :: this should not happen here though (only for verification
-# purposes
+# purposes  (data loading happens in the run file)
 
 df_m = pd.read_csv(instance_path.path_input + '/' + 'RealizedMeasures03_10.csv',
                    index_col=0)
@@ -19,17 +19,20 @@ class HARModel:
                  df,
                  future=1,
                  lags=[4, 20],
-                 feature='RV_s',  # depending on the data frame it can be adapted
-                 period_train=['20030910', '20081001'],  #pd.to_datetime('20090110', format='%Y%m%d')  # format to date
-                 period_test=['20081002', '20101231'],
-                 semi_variance=False):
+                 feature='RV_s',
+                 semi_variance=False,
+                 period_train=[pd.to_datetime('20030910', format='%Y%m%d'),
+                               pd.to_datetime('20081001', format='%Y%m%d')],
+                 period_test=[pd.to_datetime('20090910', format='%Y%m%d'),
+                              pd.to_datetime('20101001', format='%Y%m%d')]
+                 ):
         self.df = df
         self.future = future
         self.lags = lags
         self.feature = feature
+        self.semi_variance = semi_variance
         self.period_train = period_train
         self.period_test = period_test
-        self.semi_variance = semi_variance
         self.prediction_train = None  # vector (or excel export)
         self.prediction_test = None  # vector (or excel export
         self.estimators = None  # table
@@ -38,11 +41,11 @@ class HARModel:
 
     def lag_average(self):
 
-        df = self.df[['DATE', self.feature]]  # shift DATE !!!
+        df = self.df[['DATE', self.feature]]
         df['RV_t'] = df[self.feature].shift(-1)
-        df['RV_t_4'] = df[self.feature].rolling(window=4).mean()
+        df['RV_t_4'] = df[self.feature].rolling(window=self.lags[0]).mean()
 
-        rw20 = self.df[self.feature].rolling(window=20).mean()
+        rw20 = self.df[self.feature].rolling(window=self.lags[1]).mean()
 
         df['RV_t_20'] = list(
                 ((self.lags[1] * rw20) - (self.lags[0] * df.RV_t_4)) /
@@ -53,41 +56,65 @@ class HARModel:
 
         df = df.dropna().reset_index(drop=True)
 
-        df.drop(['RV_s'], axis=1, inplace=True)
+        df.drop([self.feature], axis=1, inplace=True)
 
-        assert round(df.RV_t[0:4].mean() - df.RV_t_4[4], 4) == 0, 'Error'
+        assert round(df.RV_t[0:self.lags[0]].mean() - df.RV_t_4[self.lags[0]], 12) == 0, 'Error'
 
         self.output_df = df
 
     def future_average(self):
+
         self.lag_average()
         df = self.output_df.copy()
         df_help = pd.DataFrame()
 
         for i in range(self.future):
             df_help[str(i)] = df.RV_t.shift(-(1 + i))
+        df_help = df_help.dropna()
 
         df['future'] = df_help.mean(axis=1)
+
+        df = df.dropna().reset_index(drop=True)
 
         self.output_df = df
 
         assert (self.output_df.RV_t[1:(self.future+1)].mean()
                 - self.output_df.future[0] == 0), 'Error'
 
-    # def generate_complete_data_set(self):
-    #     if self.semi_variance:
-    #         self.future_average()
-    #         df = self.output_df.copy()
-    #         df
+    def generate_complete_data_set(self):
+        if self.semi_variance:
+            self.future_average()
+            df = self.output_df.copy()
+            df = df.merge(self.df[['DATE', 'RSV_s_plus', 'RSV_s_minus']], on='DATE')
+
+        else:
+            self.future_average()
+            df = self.output_df
+
+        self.output_df = df
+
+        assert (self.output_df.RV_t.iloc[1:(self.future + 1)].mean()
+                - self.output_df.future[0] == 0), 'Error'
+
+        # add an estimation an a prediction method
+        # add an export method that saves all the results
 
 
 instance_test = HARModel(
     df=df_m,
-    future=1,
-    lags=[4, 20],
-    feature='RV_s')
+    future=3,
+    lags=[1, 20],
+    feature='RV_s',
+    semi_variance=False)
 
-instance_test.lag_average()
-instance_test.future_average()
-instance_test.output_df.head()
+instance_test.generate_complete_data_set()
+df_test = instance_test.output_df
+df_test
+
+
+plt.figure()
+plt.plot(df_test.DATE[0:500], df_test.RV_t[0:500], 'darkgreen', alpha=0.75)
+plt.plot(df_test.DATE[0:500], df_test.future[0:500], 'k-.')
+plt.show()
+
 
